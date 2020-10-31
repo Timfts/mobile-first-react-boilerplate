0d86a79d761fb9562ed6166187dc95c5836b88bc
Italo Fontes <italo5123@hotmail.com>
2020-10-31 09:17:13 -0300

    :arrow_up: updated webpack

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -28,7 +28,6 @@ module.exports = merge(commonConfig, {
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(), // enable HMR globally
-    new webpack.NamedModulesPlugin(), // prints more readable module names in the browser console on HMR updates
     new TSLintPlugin({
       files: ["../src/**/*.ts", "../src/**/*.tsx"],
     }), // linting while development
